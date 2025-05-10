61de7c287e116247632acdec60cecdea3d5932c6
vinoddevasani <vinoddevasani@gmail.com>
2025-05-10 13:12:04 +0530

    First Commit

---
+++ development/even_odd_test.py
@@ -0,0 +1,14 @@
+# Simple Python code to test if a number is even or odd
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+
+# Example usage
+num = int(input("Enter a number: "))
+check_even_odd(num)
+
+
+#https://github.com/vinoddevasani/devasaniv
